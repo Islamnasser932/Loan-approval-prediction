7fbb8944d52bbc474e1779d2d61f021f924f71dd
Islam Nasser <72976111+Islamnasser932@users.noreply.github.com>
2024-12-02 14:52:20 +0200

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -4,8 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px 
-from ydata_profiling import ProfileReport
 from streamlit_option_menu import option_menu
 from streamlit_extras.metric_cards import style_metric_cards
 
