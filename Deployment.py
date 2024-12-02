788a727b6c24787957ce8037bec9a95b6642f900
Islam Nasser <72976111+Islamnasser932@users.noreply.github.com>
2024-12-02 15:09:00 +0200

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px 
 from streamlit_option_menu import option_menu
 from streamlit_extras.metric_cards import style_metric_cards
 
